d2c971ddac45a27d0ebfb3f7d43eb06a9dac1997
Adam Booth <4rlm@protonmail.ch>
2018-06-01 14:45:56 -0500

    refactor

--- lib/utf8_sanitizer.rb
+++ lib/utf8_sanitizer.rb
@@ -1,5 +1,5 @@
 require "utf8_sanitizer/version"
-require 'utf8_sanitizer/run'
+# require 'utf8_sanitizer/run'
 require 'utf8_sanitizer/seed'
 require 'utf8_sanitizer/utf'
 require 'pry'
@@ -7,8 +7,32 @@ require 'pry'
 module Utf8Sanitizer
 
   def self.run_wrap
-    run = self::Run.new
-    result = run.import ## returns formatted urls.
+    # run = self::Run.new
+    data = import ## returns formatted urls.
+    binding.pry
+    data
   end
+
+  def self.import(args={})
+    binding.pry
+    data = { stats: nil, data: nil, file_path: nil, criteria: nil }
+    data.merge!(args)
+    keys = args.compact.keys
+
+    unless (keys & [:data, :file_path]).any?
+      data[:file_path] = Seed.new.grab_seed_file_path
+      # @data[:data] = Seed.new.grab_seed_hashes
+      data[:pollute_seeds] = true
+      unless keys.include?(:criteria)
+        data[:criteria] = Seed.new.grab_seed_web_criteria
+      end
+    end
+
+    utf_result = Utf8Sanitizer::UTF.new.validate_data(data)
+    data.merge!(utf_result)
+    binding.pry
+    data
+  end
+
 
 end
--- lib/utf8_sanitizer/run.rb
+++ lib/utf8_sanitizer/run.rb
@@ -2,29 +2,29 @@
 module Utf8Sanitizer
   class Run
 
-    def initialize
-      @crm_data = {}
-    end
-
-
-    def import(args={})
-      @crm_data = { stats: nil, data: nil, file_path: nil, criteria: nil }
-      @crm_data.merge!(args)
-      keys = args.compact.keys
-
-      unless (keys & [:data, :file_path]).any?
-        @crm_data[:file_path] = Seed.new.grab_seed_file_path
-        # @crm_data[:data] = Seed.new.grab_seed_hashes
-        @crm_data[:pollute_seeds] = true
-        unless keys.include?(:criteria)
-          @crm_data[:criteria] = Seed.new.grab_seed_web_criteria
-        end
-      end
-
-      utf_result = Utf8Sanitizer::UTF.new.validate_data(@crm_data)
-      @crm_data.merge!(utf_result)
-    end
-
+    # def initialize
+    #   @crm_data = {}
+    # end
+    # 
+    #
+    # def import(args={})
+    #   @crm_data = { stats: nil, data: nil, file_path: nil, criteria: nil }
+    #   @crm_data.merge!(args)
+    #   keys = args.compact.keys
+    #
+    #   unless (keys & [:data, :file_path]).any?
+    #     @crm_data[:file_path] = Seed.new.grab_seed_file_path
+    #     # @crm_data[:data] = Seed.new.grab_seed_hashes
+    #     @crm_data[:pollute_seeds] = true
+    #     unless keys.include?(:criteria)
+    #       @crm_data[:criteria] = Seed.new.grab_seed_web_criteria
+    #     end
+    #   end
+    #
+    #   utf_result = Utf8Sanitizer::UTF.new.validate_data(@crm_data)
+    #   @crm_data.merge!(utf_result)
+    # end
+    #
 
 
   end
