bde3a19e0a34f644f29258371dc5a0489387038b
Adam Booth <4rlm@protonmail.ch>
2018-06-30 13:58:05 -0500

    updated dependencies.

--- spec/utf8_sanitizer_spec.rb
+++ spec/utf8_sanitizer_spec.rb
@@ -3,11 +3,5 @@ require 'utf8_sanitizer'
 require 'spec_helper'
 
 RSpec.describe 'Utf8Sanitizer' do
-  it 'has a version number' do
-    expect(Utf8Sanitizer::VERSION).not_to be nil
-  end
 
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
 end
